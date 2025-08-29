425595c24e42f5560758ff60726751ba0d0ebefe
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-08-29 10:18:39 +0000

    tests: clear os environ for every test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
 """Fixtures for fed-mgr tests."""
 
+import os
 from unittest import mock
 
 import pytest
+
+
+@pytest.fixture(autouse=True)
+def clear_os_environment() -> None:
+    """Clear the OS environment."""
+    os.environ.clear()
 
 
 @pytest.fixture
