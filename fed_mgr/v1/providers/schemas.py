37676751ca8ae5e8053056400067c4f4b2143c8a
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-09-02 17:30:21 +0000

    Revert "feat: add revoke provider function and related background tasks management"
    This reverts commit b60cb35d81c41c14b127e24ce78d5de146ad01b9.

--- fed_mgr/v1/providers/schemas.py
+++ fed_mgr/v1/providers/schemas.py
@@ -2,7 +2,6 @@
 
 import urllib.parse
 import uuid
-from datetime import date
 from enum import Enum
 from typing import Annotated
 
@@ -107,13 +106,6 @@ class ProviderInternal(SQLModel):
     status: Annotated[
         ProviderStatus,
         Field(default=ProviderStatus.draft, description="Resource provider status"),
-    ]
-    expiration_date: Annotated[
-        date | None,
-        Field(
-            default=None,
-            description="Date of when the provider will be no more available",
-        ),
     ]
 
 
