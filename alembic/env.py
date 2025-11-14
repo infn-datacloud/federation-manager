fd1418f693b2e119c5aecbc5430c9a16de8bc03a
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-11-14 17:44:20 +0000

    Add first migration and update env.py to load sqlalchemy url from settings

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,13 @@
 """cript that is run whenever the alembic migration tool is invoked."""
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
+from sqlmodel import SQLModel
 
 from alembic import context
+from fed_mgr.config import get_settings
+from fed_mgr.v1.models import *  # noqa: F403
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +42,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    settings = get_settings()
+    url = settings.DB_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,16 +62,17 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    settings = get_settings()
+    data = config.get_section(config.config_ini_section, {})
+    data["sqlalchemy.url"] = settings.DB_URL
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        data,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
